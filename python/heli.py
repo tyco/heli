0eddf55b725f52a0e67fb95dd7567dbab1ba35e0
Tyler <tyler.cordrey@gmail.com>
2010-11-01 22:58:37 -0400

    a basically working parser!

--- python/heli.py
+++ python/heli.py
@@ -1,68 +1,63 @@
-import scipy.io.numpyio as npio
-import struct
 import numpy as np
 
+shortdtype = np.dtype('int16')
 
-def parse(block):
-    lastcount = -1
+def parse(data, block, scancount):
     index = 0
     blocklen = len(block)
-
-    ret={}
+    print "blocklen: %d"  % blocklen
+    
+    skipped = 0
     
     while(blocklen - index > 3):
-        while ord(block[index]) & 0xF0 != 0x50:
-	    index = index + 1
-        scantype = ord(block[index]) & 0xF
+        typebyte = ord(block[index])
+        if typebyte & 0xF0 != 0x50:
+            index = index + 1
+            skipped = skipped + 1            
+            continue
+        scantype = typebyte & 0xF
         index = index+1
         counter = ord(block[index])
-        if lastcount >= 0:
-            if counter != lastcount+1 and not (lastcount == 255 and counter == 0):
-                print "bad scan count: %d to %d" % (lastcount, counter)
+        if scancount >= 0:
+            if counter != scancount+1 and not (scancount == 255 and counter == 0):
+                print "bad scan count: %d to %d" % (scancount, counter)
         index = index+1
         scanlen = ord(block[index])
+        index = index+1
         if blocklen - index < scanlen:
+            # back up to the start of the scan header
+            index = index - 3
             break
 
+        scancount = counter
         if scanlen % 2 != 0 : print "odd scan length!"
-	numscans = scanlen / 2
+        num_chs = scanlen / 2
+        if(num_chs != 11): print num_chs
 
-	datalists=[]
-	if not ret.has_key(scantype):
-		ret[scantype] = datalists
-	else:
-		datalists = ret[scantype]
-	
-	for i in range(numscans - len(datalists)):
-		datalists.append([])
-
-	for i in range(numscans):
-		datalists[i].append(struct.unpack('<h', block[numscans:numscans+2]))
-    return (ret, block[index:])
+        new_scan = np.ndarray(shape=(1,num_chs), dtype=np.int16, buffer=block[index:index+num_chs*2])
+        index += num_chs*2
+        
+        if data.has_key(scantype):
+            #print "new: %s, old: %s" % (str(new_scan.shape), str(data[scantype].shape))
+            data[scantype] = np.vstack((data[scantype], new_scan))
+        else:
+            data[scantype] = new_scan
+    
+    return (block[index:], scancount)
 
     
-    
-f=open('heli_data/capture', 'rb')
-
+import os
+print os.curdir
+f=open('../heli_data/imu_capture.1', 'rb')
 
 masterdata = {}
 leftover = ""
+scancount = -1
 r = f.read(8192)
 while(len(r) > 0):
-	(newdata, leftover) = parse(leftover + r)
-
-	for key in newdata:
-		if not masterdata.has_key(key):
-			masterdata[key] = newdata[key]
-		else:
-			for key in newdata:
-				masterdata[key] += newdata[key]
-	r = f.read(8192)
-
-for scantype in masterdata:
-	masterdata[scantype] = np.array(masterdata[key])
-	print masterdata[scantype].shape
+    (leftover, scancount) = parse(masterdata, leftover + r, scancount)
+    print len(leftover)
+    r = f.read(8192)
 
 #r=npio.fread(f, 1024, 'i')
-
 
